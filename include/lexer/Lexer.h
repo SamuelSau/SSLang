eeada952758147a87f17489cc191406130c9f663
unknown <samuelysau@gmail.com>
2024-03-19 14:48:38 -0400

    first commit

---
+++ include/lexer/Lexer.h
@@ -0,0 +1,61 @@
+// Lexer.h
+#ifndef LEXER_H
+#define LEXER_H
+
+#include <string>
+#include <string_view>
+
+class Token {
+public:
+    enum class Kind {
+        Number, Identifier, LeftParen, RightParen, LeftSquare, RightSquare,
+        LeftCurly, RightCurly, LessThan, GreaterThan, Equal, Plus, Minus,
+        Asterisk, Slash, Hash, Dot, Comma, Colon, Semicolon, SingleQuote,
+        DoubleQuote, Comment, Pipe, End, Unexpected,
+    };
+
+    Token(Kind kind) noexcept;
+    Token(Kind kind, const char* beg, std::size_t len) noexcept;
+    Token(Kind kind, const char* beg, const char* end) noexcept;
+
+    Kind kind() const noexcept;
+    void kind(Kind kind) noexcept;
+    bool is(Kind kind) const noexcept;
+    bool is_not(Kind kind) const noexcept;
+    bool is_one_of(Kind k1, Kind k2) const noexcept;
+
+    template <typename... Ts>
+    bool is_one_of(Kind k1, Kind k2, Ts... ks) const noexcept;
+
+    std::string_view lexeme() const noexcept;
+    void lexeme(std::string_view lexeme) noexcept;
+
+private:
+    Kind m_kind{};
+    std::string_view m_lexeme{};
+};
+
+class Lexer {
+public:
+    Lexer(const char* beg) noexcept;
+
+    Token next() noexcept;
+
+private:
+    Token identifier() noexcept;
+    Token number() noexcept;
+    Token slash_or_comment() noexcept;
+    Token atom(Token::Kind) noexcept;
+
+    char peek() const noexcept { return *m_beg; }
+    char get() noexcept { return *m_beg++; }
+    const char* m_beg = nullptr;
+};
+
+bool is_space(char c) noexcept;
+bool is_digit(char c) noexcept;
+bool is_identifier_char(char c) noexcept;
+
+std::ostream& operator<<(std::ostream& os, const Token::Kind& kind);
+
+#endif // LEXER_H
---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include "lexer/Lexer.h"
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <cstddef>
+#include <iterator>
+
+int main() {
+  auto code =
+      "x = 2\n"
+      "// This is a comment.\n"
+      "var x\n"
+      "var y\n"
+      "var f = function(x, y) { sin(x) * sin(y) + x * y; }\n"
+      "der(f, x)\n"
+      "var g = function(x, y) { 2 * (x + der(f, y)); } // der(f, y) is a "
+      "matrix\n"
+      "var r{3}; // Vector of three elements\n"
+      "var J{12, 12}; // Matrix of 12x12 elements\n"
+      "var dot = function(u{:}, v{:}) -> scalar {\n"
+      "          return u[i] * v[i]; // Einstein notation\n"
+      "}\n"
+      "var norm = function(u{:}) -> scalar { return sqrt(dot(u, u)); }\n"
+      "<end>";
+
+  Lexer lex(code);
+  for (auto token = lex.next(); !token.is_one_of(Token::Kind::End, Token::Kind::Unexpected);
+       token = lex.next()) {
+    std::cout << std::setw(12) << token.kind() << " |" << token.lexeme()
+              << "|\n";
+  }
+}
